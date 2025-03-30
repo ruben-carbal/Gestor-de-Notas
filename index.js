5f0e7c61e8ce1d21582d27f695a87ae1a13292a2
Ruben <rbenbal43@gmail.com>
2025-03-30 13:39:33 -0600

    se fue la luz, después conecto la base de datos

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const sqlite = require('node:sqlite');
+const database = new DatabaseSync(':memory:');
 
 const app = express();
 
 app.set('view-engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+database.exec('CREATE TABLE notes (id INTEGER PRIMARY KEY AUTO INCREMENT, note TEXT)');
+
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.render('index.ejs');
+})
+
+app.post('/notes', (req, res) => {
+    const insert = database.prepare('INSERT INTO notes (note) VALUES(?)');
+
 })
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log("server running...");
+    console.log(`server running on port ${PORT}...`);
 })
